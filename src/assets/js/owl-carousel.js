8d8b6275996a9d87e6da2947b91e86c48e18f475
Duygu Temur <duygutemur@archis.company>
2021-08-01 15:06:30 +0300

    add slider section

--- src/assets/js/owl-carousel.js
+++ src/assets/js/owl-carousel.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 $('.owl-carousel').owlCarousel({
     loop:false,
     margin:20,
-    center: true,
     nav:true,
+    autoWidth:true,
+    navText: ["<img src='../assets/svg/left-arrow.svg'>","<img src='../assets/svg/right-arrow.svg'>"],
     responsive:{
         0:{
             items:1
